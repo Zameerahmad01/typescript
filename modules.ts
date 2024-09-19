6ae3b3e5dbf6067b74644fdde1495dfbade4fe8c
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-09-19 22:04:26 +0500

    modules types in typescript

---
+++ modules.ts
@@ -0,0 +1,24 @@
+// modules
+
+// *named exports
+//named exports are used to export multiple classes, functions, or variables from a module.
+//named exports are imported with the same name they were exported with using curly braces.
+// todo export class MyClass {
+// todo  constructor(public name: string) {}
+// }
+
+// *default exports
+//default exports are used to export a single class, function, or variable from a module.
+//default exports are imported without curly braces and without the need to specify the name of the exported class, function, or variable.
+// todo export default class Class {
+//  todo constructor(public name: string) {}
+// }
+
+// *importing named exports
+// todo import { MyClass } from "./modules";
+
+// *	importing default exports
+// todo import Class from "./modules";
+
+// *importing both named and default exports
+// todo import { MyClass, default as Class } from "./modules";
